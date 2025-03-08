f033823c34d342244f0f7e69b0e1044df157c481
Abhijeet Bagade <bagadeabhijeet408@gmail.com>
2025-03-08 17:15:46 +0530

    Add files via upload

---
+++ CollaborativeApp.java
@@ -0,0 +1,12 @@
+package com.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollaborativeApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollaborativeApp.class, args);
+    }
+}
